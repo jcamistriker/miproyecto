3712eb28ac911183182e80a002271ff26139333c
jcamistriker <camilostriker@gmail.com>
2019-02-28 17:54:32 -0500

    A

---
+++ animacion.js
@@ -0,0 +1,81 @@
+
+function apretar(){
+	alert("Hola mundo");
+
+};
+
+function encima(){
+
+	alert("El raton esta encima");
+};
+
+function fuera(){
+	alert("El raton esta afuera");
+
+};
+
+
+
+/* =====================================
+keydown
+keypress
+keyup
+=========================================== */ 
+
+/* =====================================
+window.addEventListener("keydown", function(){alert("accion")});
+
+*/
+
+
+/*
+window.addEventListener("keydown", function(event){
+		alert(String.fromCharCode(event.keyCode))
+}
+);
+
+=========================================== */ 
+
+
+/* =====================================
+
+serInterval - de forma indefinida
+setTimeout - se ejecuta una sola vez
+
+=========================================== */ 
+
+
+/* =====================================
+
+var tiempo = setInterval(
+	function(){document.write("UNO <br>")},
+		2000
+);
+
+=========================================== */ 
+
+
+
+
+ 
+
+
+
+/* =====================================
+
+=========================================== */ 
+
+
+
+
+
+
+
+/* =====================================
+
+=========================================== */ 
+
+
+
+
+
